16b3a90206fa68a3580e85267832ea4665c50aa0
bushra4468 <bushra.hussain.student@btechnyc.org>
2022-02-20 18:15:24 -0500

    Create class.cs

---
+++ class.cs
@@ -0,0 +1,31 @@
+using System;
+
+customerspace ConsoleApplication
+{
+    class Student
+    {
+        public string customer;
+        public int id;
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Supra = new Student();
+            Supra.customer = "Supra";
+            Supra.id = 1;
+
+            var BMW = new Student();
+            BMW.customer = "BMW";
+            BMW.id = 2;
+
+            var Toyota = new student();
+            Toyota.customer = "Toyota";
+            Toyota.id = 3;
+
+        }
+    }
+
+}
