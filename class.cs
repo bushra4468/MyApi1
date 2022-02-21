34e6054ee8c2c28e75d5bbb3587c8e24451143c8
Your Name <bushrahussain4468@gmail.com>
2022-02-20 20:47:47 -0500

    Update class.cs

--- class.cs
+++ class.cs
@@ -1,29 +1,84 @@
 using System;
 
-customerspace ConsoleApplication
+namespace ConsoleApplication
 {
-    class Student
+    class Name
     {
         public string customer;
-        public int id;
-    };
+        public int brand;
+    }
+
+    interface MyInterface
+    {
+      string customer {get; set; }
+      int brand {get; set; }
+
+    }
+
+    class Name : MyInterface
+    {
+      public string customer {get; set; }
+    }
+
+    public class App
+    {
+      public string Set;
+    }
+
+
+    public class AppSet
+    {
+      public delegate void AppSetterEventHandler(object source, EventArgss args);
+
+      public event AppSetterEventHandler AppSetter;
+
+      public void ReadyApp(Order order)
+      {
+          Console.WriteLine($"Preparing your car '{order.Set}, hold on a moment...:) '");
+          Thread.Sleep(4000);
+
+          OnAppSetter();
+      }
+
+      protected virtual void OnAppSetter()
+      {
+          if (AppSetter != null)
+              AppSetter(this, null);
+      }
+
+
+     }
+
+     public class AppDone
+     {
+       public void OnAppSetter(object source, EventArgs eventArgs)
+       {
+         Console.WriteLine("Your car is ready for pickup");
+       }
+     }
+
 
     class Program
     {
         static void Main(string[] args)
         {
 
-            var Supra = new Student();
+            var Supra = new Name{Set = "SuperCar in red"};
+            var orderCar = new AppSet();
+            var appSer = new AppDone();
+            orderCar.AppSetter += appSer.OnAppSetter;
+            orderCar.OnAppSetter(name);
+            Console.Readkey();
             Supra.customer = "Supra";
-            Supra.id = 1;
+            Supra.brand = 1;
 
-            var BMW = new Student();
+            var BMW = new Name();
             BMW.customer = "BMW";
-            BMW.id = 2;
+            BMW.brand = 2;
 
-            var Toyota = new student();
+            var Toyota = new Name();
             Toyota.customer = "Toyota";
-            Toyota.id = 3;
+            Toyota.brand = 3;
 
         }
     }
